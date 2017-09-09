7e2fa8793977dff01061e6859b6fbb8ec842c333
Ilyas <31530783+Pilot669@users.noreply.github.com>
2017-09-09 12:18:36 +0300

    Домашка про температуру

---
+++ src/com/company/Temp.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Temp {
+    void convert() {
+        TemperatureConverter temperatura = new TemperatureConverter();
+        String result = temperatura.convertTemp(80, 'F');
+        System.out.println("result " + result);
+        //temperatura.convertTemp(50, 'C');
+    }
+}
---
+++ src/com/company/TemperatureConverter.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class TemperatureConverter{
+
+    public String convertTemp(int temperature, char convertTo){
+        /* Если аргумент convertTo равен F, то температура
+        должна быть преобразована в Фаренгейты*/
+        if (convertTo == 'C') {
+            temperature = temperature * 9 / 5 - 32;
+            return temperature + "F";
+        } else if (convertTo == 'F') {
+            temperature = temperature - 32 * 9 / 5;
+            return temperature + "C";
+        }
+        return "Error ";
+    }
+
+}
